ccbdfbe2049fd0e2cd5f47f4624eeb8b4827fb86
Ferenc Hechler <github@fh.anderemails.de>
2024-12-19 06:27:52 +0100

    feri day 19 in java

--- exchange/day19/feri/java/Y24Day19.java
+++ exchange/day19/feri/java/Y24Day19.java
@@ -17,8 +17,11 @@ public class Y24Day19 {
 	public static class World {
 		String[] towels;
 		
+		Map<String, Long> cache;
+		
 		public World(String[] towels) {
 			this.towels = towels;
+			this.cache = new HashMap<>();
 		}
 
 		public boolean checkPattern(String pattern) {
@@ -33,6 +36,24 @@ public class Y24Day19 {
 				}
 			}
 			return false;
+		}
+
+		public long countPatterns(String pattern) {
+			if (pattern.isEmpty()) {
+				return 1;
+			}
+			if (cache.containsKey(pattern)) {
+				return cache.get(pattern);
+			}
+			long result = 0;
+			for (String towel:towels) {
+				if (pattern.startsWith(towel)) {
+					long cntPatterns = countPatterns(pattern.substring(towel.length()));
+					result += cntPatterns;
+				}
+			}
+			cache.put(pattern, result);
+			return result;
 		}
 	}
 	
@@ -61,6 +82,22 @@ public class Y24Day19 {
 
 	public static void mainPart2(String inputfile) throws FileNotFoundException {
 		
+		long numberPatterns = 0;
+		try (Scanner scanner = new Scanner(new File(inputfile))) {
+			String line = scanner.nextLine().trim();
+			String[] towels = line.split(", ");
+			World world = new World(towels);
+			while (scanner.hasNext()) {
+				line = scanner.nextLine().trim();
+				if (line.isBlank()) {
+					continue;
+				}
+				long cntPatterns = world.countPatterns(line);
+				numberPatterns = numberPatterns + cntPatterns; 
+				System.out.println(cntPatterns+" for "+line);
+			}
+		}
+		System.out.println("number of patterns: "+numberPatterns);
 	}
 
 
@@ -68,12 +105,12 @@ public class Y24Day19 {
 	public static void main(String[] args) throws FileNotFoundException {
 		System.out.println("--- PART I  ---");
 //		mainPart1("exchange/day19/feri/input-example.txt");
-		mainPart1("exchange/day19/feri/input.txt");
+//		mainPart1("exchange/day19/feri/input.txt");
 		System.out.println("---------------");
 		System.out.println();
 		System.out.println("--- PART II ---");
-		mainPart2("exchange/day19/feri/input-example.txt");
-//		mainPart2("exchange/day19/feri/input.txt");    
+//		mainPart2("exchange/day19/feri/input-example.txt");
+		mainPart2("exchange/day19/feri/input.txt");       // not 407858650
 		System.out.println("---------------");
 	}
 
