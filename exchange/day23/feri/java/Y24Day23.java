f28af74f7e9d8a5137e92ec4f8db808292a2dfdb
Ferenc Hechler <ferenc.hechler@telekom.de>
2024-12-23 11:38:30 +0100

    day 23 part 1

--- exchange/day23/feri/java/Y24Day23.java
+++ exchange/day23/feri/java/Y24Day23.java
@@ -1,23 +1,34 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.LinkedHashSet;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.Set;
 
 /**
- * see: https://adventofcode.com/2024/day/21
+ * see: https://adventofcode.com/2024/day/23
  *
  */
 public class Y24Day23 {
 	
 	
+	record ThreeNet(String c1, String c2, String c3) {
+		public static ThreeNet createSorted(String ca, String cb, String cc) {
+			String[] sortedC = new String[] {ca, cb, cc};
+			Arrays.sort(sortedC);
+			return new ThreeNet(sortedC[0], sortedC[1], sortedC[2]);
+		}
+		@Override
+		public String toString() {
+			return c1+","+c2+","+c3;
+		}
+	}
+	
 	public static void mainPart1(String inputfile) throws FileNotFoundException {
 
+		Map<String, Set<String>> connections = new HashMap<>();
 		try (Scanner scanner = new Scanner(new File(inputfile))) {
 			while (scanner.hasNext()) {
 				String line = scanner.nextLine().trim();
@@ -26,7 +37,23 @@ public class Y24Day23 {
 				}
 				String[] computers = line.split("-");
 				System.out.println(computers[0] + " -- " + computers[1]);
+				connections.computeIfAbsent(computers[0], (k)->new LinkedHashSet<>()).add(computers[1]);
+				connections.computeIfAbsent(computers[1], (k)->new LinkedHashSet<>()).add(computers[0]);
 			}
+			Set<ThreeNet> t3Nets = new LinkedHashSet<>();
+			for (String computer1:connections.keySet()) {
+				if (computer1.startsWith("t")) {
+					for (String computer2:connections.get(computer1)) {
+						for (String computer3:connections.get(computer2)) {
+							if (connections.get(computer3).contains(computer1)) {
+								t3Nets.add(ThreeNet.createSorted(computer1, computer2, computer3));
+							}
+						}
+					}
+				}
+			}
+//			System.out.println(t3Nets);
+			System.out.println(t3Nets.size());
 		}
 	}
 
@@ -38,8 +65,8 @@ public class Y24Day23 {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		System.out.println("--- PART I  ---");
-		mainPart1("exchange/day23/feri/input-example.txt");
-//		mainPart1("exchange/day23/feri/input.txt");     
+//		mainPart1("exchange/day23/feri/input-example.txt");
+		mainPart1("exchange/day23/feri/input.txt");     
 		System.out.println("---------------");
 		System.out.println();
 		System.out.println("--- PART II ---");
