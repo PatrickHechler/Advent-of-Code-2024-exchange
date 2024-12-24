db6d1006aeb0042337584ac0096c25189f169bb9
Ferenc Hechler <github@fh.anderemails.de>
2024-12-24 13:02:20 +0100

    part2 start

--- exchange/day24/feri/java/Y24Day24.java
+++ exchange/day24/feri/java/Y24Day24.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -15,6 +16,9 @@ public class Y24Day24 {
 	
 	static final String START_RX = "^([a-z0-9]+): (0|1)$";
 	static final String OPERATION_RX = "^([a-z0-9]+) (AND|OR|XOR) ([a-z0-9]+) -> ([a-z0-9]+)$";
+
+	static final long XY_MASK = (1L<<44)-1L;
+	static final long Z_MASK = (1L<<45)-1L;
 	
 	record Rule (String reg1, String op, String reg2, String regTarget) {
 		@Override
@@ -29,6 +33,25 @@ public class Y24Day24 {
 		public Circuit() {
 			rules = new LinkedHashMap<>();
 			regs = new LinkedHashMap<>();
+		}
+		public Circuit swap(String ruleTarget1, String ruleTarget2) {
+			Circuit result = new Circuit();
+			Rule rule1 = rules.get(ruleTarget1);
+			Rule rule2 = rules.get(ruleTarget2);
+			Rule swap1 = new Rule(rule1.reg1, rule1.op, rule1.reg2, rule2.regTarget);
+			Rule swap2 = new Rule(rule2.reg1, rule2.op, rule2.reg2, rule1.regTarget);
+			for (Rule rule:rules.values()) {
+				if (rule.regTarget.equals(ruleTarget1)) {
+					result.addRules(swap1);
+				}
+				else if (rule.regTarget.equals(ruleTarget2)) {
+					result.addRules(swap2);
+				}
+				else {
+					result.addRules(rule);
+				}
+			}
+			return result;
 		}
 		public void setRegValue(String reg, int value) {
 			regs.put(reg, value);
@@ -49,7 +72,7 @@ public class Y24Day24 {
 			List<String> sortedRegs = new ArrayList<>(regs.keySet());
 			Collections.sort(sortedRegs);
 			for (String reg:sortedRegs) {
-				result.append(reg+": "+regs.get(reg));
+				result.append(reg+": "+regs.get(reg)).append("\n");
 			}
 			return result.toString();
 		}
@@ -108,6 +131,38 @@ public class Y24Day24 {
 		public long getBinValue(String prefix) {
 			return Long.parseLong(getBinString(prefix), 2);
 		}
+		public void clearRegs() {
+			regs.clear();
+		}
+		public long getZ() {
+			return getBinValue("z");
+		}
+		public void setX(long x) {
+			setReg("x",x, 44);
+		}
+		public void setY(long y) {
+			setReg("y",y, 44);
+		}
+		private void setReg(String prefix, long value, int maxBit) {
+			long bit = 1L;
+			for (int i=0; i<=maxBit; i++) {
+				int b = 0;
+				if ((value & bit) != 0L) {
+					b = 1;
+				}
+				String num = Integer.toString(i);
+				if (i<10) {
+					num = "0"+num;
+				}
+				regs.put(prefix+num, b);
+				bit = bit<<1;
+			}
+		}
+		public List<String> getRuleTargets() {
+			List<String> result = new ArrayList<>(rules.keySet());
+			Collections.sort(result);
+			return result;
+		}
 	}
 	
 	public static void mainPart1(String inputfile) throws FileNotFoundException {
@@ -140,31 +195,129 @@ public class Y24Day24 {
 			System.out.println(circuit);
 			System.out.println("RESULT: "+circuit.getBinString("z"));
 			System.out.println("      = "+circuit.getBinValue("z"));
+		}
+	}
+
+	public static void mainPart2(String inputfile) throws FileNotFoundException {
+		Circuit circuit = new Circuit();
+		try (Scanner scanner = new Scanner(new File(inputfile))) {
+			while (scanner.hasNext()) {
+				String line = scanner.nextLine().trim();
+				if (line.isBlank()) {
+					continue;
+				}
+				if (line.matches(START_RX)) {
+				}
+				else if (line.matches(OPERATION_RX)) {
+					String reg1 = line.replaceFirst(OPERATION_RX, "$1");
+					String op = line.replaceFirst(OPERATION_RX, "$2");
+					String reg2 = line.replaceFirst(OPERATION_RX, "$3");
+					String regTarget = line.replaceFirst(OPERATION_RX, "$4");
+					circuit.addRules(new Rule(reg1, op, reg2, regTarget));
+				}
+				else {
+					throw new RuntimeException("unknown line");
+				}
+			}
+			System.out.println(circuit);
+			ErrStats es = findWeakBits(circuit, 10000);
+			System.out.println("Error Stats: "+es);
+			
+			long x = es.errX;
+			long y = es.errY;
+			int bestCntErrBits = es.cntErrBits;
+			String bestSwap1 = null;
+			String bestSwap2 = null;
+			List<String> ruleTargets = circuit.getRuleTargets();
+			for (String target1:ruleTargets) {
+				for (String target2:ruleTargets) {
+					if (target1.equals(target2)) {
+						continue;
+					}
+					Circuit swappedCircuit = circuit.swap(target1, target2);
+					int cntErrBits = countErrBits(swappedCircuit, x, y);
+					if (cntErrBits <= bestCntErrBits) {
+						bestCntErrBits = cntErrBits;
+						bestSwap1 = target1;
+						bestSwap2 = target2;
+						System.out.println("CntErrBits: "+cntErrBits+" "+target1+" <-> "+target2);
+					}
+							
+				}
+			}
 			
 		}
 	}
 
-	private static String z(int i) {
-		String result = Integer.toString(i);
-		if (i<10) {
-			result = "0"+result;
+	static Random rand = new Random(1);
+
+	static record ErrStats(long errX, long errY, long errMaskZ, int cntErrBits, long weakBits, int cntWeakBits) {}
+	
+	private static ErrStats findWeakBits(Y24Day24.Circuit circuit, int iterations) {
+		long maxErrBits = 0L;
+		int maxCntErrBits = 0;
+		long maxErrBitsX = 0L;
+		long maxErrBitsY = 0L;
+		long weakBits = 0L;
+		long lastWeakBits = 0L;
+		for (int i=0; i<iterations; i++) {
+			long x = rand.nextLong() & XY_MASK;
+			long y = rand.nextLong() & XY_MASK;
+			long z = x + y;
+			circuit.clearRegs();
+			circuit.setX(x);
+			circuit.setY(y);
+			circuit.calcAll();
+			long zComp = circuit.getZ();
+			long errBits = zComp ^ z;
+			int cntErrBits = Long.bitCount(errBits);
+			if (cntErrBits>maxCntErrBits) {
+				maxCntErrBits = cntErrBits;
+				maxErrBits = errBits;
+				maxErrBitsX = x;
+				maxErrBitsY = y;
+			}
+			weakBits = weakBits | errBits;
+			if (weakBits != lastWeakBits) {
+				lastWeakBits = weakBits;
+				System.out.println(i+": "+Long.toString(weakBits, 2));
+			}
 		}
-		return "z"+result;
+		List<String> result = new ArrayList<>();
+		for (int i=0; i<45; i++) {
+			if (((1L<<i) & weakBits) != 0L) {
+				String num = Integer.toString(i);
+				if (i<10) {
+					num = "0"+num;
+				}
+				result.add("z"+num);
+			}
+		}
+		return new ErrStats(maxErrBitsX, maxErrBitsY, maxErrBits, maxCntErrBits, weakBits, Long.bitCount(weakBits));
 	}
 
-	public static void mainPart2(String inputfile) throws FileNotFoundException {
+	private static int countErrBits(Circuit circuit, long x, long y) {
+		long z = x + y;
+		circuit.clearRegs();
+		circuit.setX(x);
+		circuit.setY(y);
+		circuit.calcAll();
+		long zComp = circuit.getZ();
+		long errBits = zComp ^ z;
+		int cntErrBits = Long.bitCount(errBits);
+		return cntErrBits;
 	}
 
-
+	
 	public static void main(String[] args) throws FileNotFoundException {
 		System.out.println("--- PART I  ---");
 //		mainPart1("exchange/day24/feri/input-example.txt");
-		mainPart1("exchange/day24/feri/input.txt");     
+//		mainPart1("exchange/day24/feri/input.txt");     
 		System.out.println("---------------");
 		System.out.println();
 		System.out.println("--- PART II ---");
-		mainPart2("exchange/day24/feri/input-example.txt");
-//		mainPart2("exchange/day24/feri/input.txt");     //
+//		mainPart2("exchange/day24/feri/input-example.txt");
+		mainPart2("exchange/day24/feri/input.txt");     //
 		System.out.println("---------------");
 	}
 
